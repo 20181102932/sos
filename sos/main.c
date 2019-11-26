07fa5c29e321680a96c418115dbc1883cc11831b
20181102932 <908907990@qq.com>
2019-11-26 16:47:38 +0800

    Initial Commit

---
+++ sos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sos
+//
+//  Created by s20181102932 on 2019/11/26.
+//  Copyright © 2019 wjx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

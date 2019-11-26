d4e69c1b491f0c53a441d737bcfb44c1452230ca
20181102932 <908907990@qq.com>
2019-11-26 16:48:28 +0800

    Update main.c

--- sos/main.c
+++ sos/main.c
@@ -10,6 +10,26 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    void setup() {
+        // put your setup code here, to run once:
+        pinMode(13,OUTPUT);
+    }
+    
+    void loop() {
+        // put your main code here, to run repeatedly:
+        for(int i=0;i<3;i++)
+        {
+            digitalWrite(13,HIGH);
+            delay(250);
+            digitalWrite(13,LOW);
+            delay(250);
+        }
+        for(int i=0;i<3;i++)
+        {
+            digitalWrite(13,HIGH);
+            delay(500);
+            digitalWrite(13,LOW);
+            delay(500);
+        }
+    }
 }
